5f73bc7a8c51ff30c400fdc76455d5c63d4f1216
Sergej Orlov <wladimirych@gmail.com>
2014-10-18 21:10:55 +0400

    Validate map zoom and center when setting from hash

--- src/leaflet_plugins/hash_state/Leaflet.Map.js
+++ src/leaflet_plugins/hash_state/Leaflet.Map.js
@@ -20,13 +20,16 @@
         },
 
         _unserializeState: function(values) {
-            if (!values || values.length != 3)
+            if (!values || values.length != 3) {
                 return false;
+            }
             var zoom = parseInt(values[0], 10),
                 lat  = parseFloat(values[1]),
                 lng  = parseFloat(values[2]);
-            if (isNaN(zoom) || isNaN(lat) || isNaN(lng))
-                return false;
+            if (isNaN(zoom) || isNaN(lat) || isNaN(lng) || zoom < 0 || zoom > 32 || lat < -90 || lat > 90 || lng < -180 || lng > 180)
+                {
+                    return false;
+                }
             this._updating_state = true;
             this.setView([lat, lng], zoom);
             this._updating_state = false;
