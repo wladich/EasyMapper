d43fbc1f9de5c14ecc52faaa3a5ceb1a933c4cfb
Sergej Orlov <wladimirych@gmail.com>
2014-10-29 10:33:38 +0300

    Added map layer from marshruty.ru

--- src/layers.js
+++ src/layers.js
@@ -15,7 +15,8 @@ window.layers.getBaseMaps = function getBaseMaps() {
         'Google': new L.Google('ROADMAP', {code: 'G', scaleDependent: true}),
         'Google Sat': new L.Google('SATELLITE', {code: 'L'}),
         'Google Sat Hybrid': new L.Google('HYBRID', {code: 'H', scaleDependent: true}),
-
+        'marshruty.ru': L.tileLayer('http://maps.marshruty.ru/ml.ashx?x={x}&y={y}&z={z}&i=1&al=1',
+                                      {code: 'M', maxNativeZoom: 18, noCors: true}),
         'Topomapper 2km': L.tileLayer('http://maps.atlogis.com/cgi-bin/tilecache-2.11/tilecache.py/1.0.0/topomapper_gmerc/{z}/{x}/{y}.jpg',
                                       {code: 'T', maxNativeZoom: 12, noCors: true})
     };
