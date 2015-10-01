b888eb08382fddda61dea2ee5016019714f174e7
Sergej Orlov <wladimirych@gmail.com>
2015-10-02 01:45:27 +0300

    Updated topomapper url

--- src/layers.js
+++ src/layers.js
@@ -25,7 +25,7 @@ window.layers.getBaseMaps = function getBaseMaps() {
         'marshruty.ru': L.tileLayer('http://maps.marshruty.ru/ml.ashx?x={x}&y={y}&z={z}&i=1&al=1',
                                       {code: 'M', maxNativeZoom: 18, noCors: true, scaleDependent: true}),
         'Topomapper 1km': L.tileLayer('http://144.76.234.108//cgi-bin/ta/tilecache.py/1.0.0/topomapper_v2/{z}/{x}/{y}.jpg',
-                                      {code: 'T', maxNativeZoom: 14, noCors: true}),
+                                      {code: 'T', maxNativeZoom: 13, noCors: true}),
     };
 };
 
