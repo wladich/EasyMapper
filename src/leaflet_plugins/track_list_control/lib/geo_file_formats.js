e7d0e79080e82eed9fdc97ff26dac64e317de98b
Sergej Orlov <wladimirych@gmail.com>
2015-12-12 22:55:58 +0300

    tracks control: fixed import of yandex ruler from url, disabled import of yandex lines from page content

--- src/leaflet_plugins/track_list_control/lib/geo_file_formats.js
+++ src/leaflet_plugins/track_list_control/lib/geo_file_formats.js
@@ -224,17 +224,17 @@
         return {error: error, points: points};
     }
 
-/*
+
     function parseYandexRulerUrl(s) {
-        var re = /^\s*https?:\/\/maps\.yandex\..+[?&]rl=([^&]+).*?$/;
-        if (!(re.test(s))) {
+        var re = /maps\.yandex\..+[?&]rl=([^&]+)/;
+        var m = re.exec(s);
+        if (!m) {
             return null;
         }
-        s = s.replace(re, "$1");
-        var res = parseYandexRulerString(s);
+        var res = parseYandexRulerString(m[1]);
         return [{name: 'Yandex ruler', error: res.error, tracks: [res.points]}];
     }
-*/
+
 
     function parseZip(txt, name) {
         var unzipper = new JSUnzip(txt);
@@ -260,62 +260,62 @@
         return geodata_array;
     }
 
-    function parseYandexMap(txt) {
-        var start_tag = '<script id="vpage" type="application/json">';
-        var json_start = txt.indexOf(start_tag);
-        if (json_start === -1) {
-            return null;
-        }
-        json_start += start_tag.length;
-        var json_end = txt.indexOf('</script>', json_start);
-        if (json_end === -1) {
-            return null;
-        }
-        var map_data = txt.substring(json_start, json_end);
-        map_data = JSON.parse(map_data);
-        console.log(map_data);
-        if (!('request' in map_data)) {
-            return null;
-        }
-        var name = 'YandexMap';
-        var segments = [];
-        var error;
-        if (map_data.vpage && map_data.vpage.data && map_data.vpage.data.objects && map_data.vpage.data.objects.length) {
-            var mapName = ('' + (map_data.vpage.data.name || '')).trim();
-            if (mapName.length > 3) {
-                name = '';
-            } else if (mapName.length) {
-                name += ': ';
-            }
-            name += fileutils.decodeUTF8(mapName);
-            map_data.vpage.data.objects.forEach(function(obj){
-                if (obj.pts && obj.pts.length) {
-                    var segment = [];
-                    for (var i=0; i< obj.pts.length; i++) {
-                        var pt = obj.pts[i];
-                        var lng = parseFloat(pt[0]);
-                        var lat = parseFloat(pt[1]);
-                        if (isNaN(lat) || isNaN(lng)) {
-                            error = 'CORRUPT';
-                            break;
-                        }
-                        segment.push({lat: lat, lng:lng});
-                    }
-                    if (segment.length) {
-                        segments.push(segment);
-                    }
-                }
-            });
-        }
-        if (map_data.request.args && map_data.request.args.rl) {
-            var res = parseYandexRulerString(map_data.request.args.rl);
-            error = error || res.error;
-            if (res.points && res.points.length) {
-                segments.push(res.points);
-            }
-        }
-        return [{name: name, error: error, tracks: segments}];
-    }
+    // function parseYandexMap(txt) {
+    //     var start_tag = '<script id="vpage" type="application/json">';
+    //     var json_start = txt.indexOf(start_tag);
+    //     if (json_start === -1) {
+    //         return null;
+    //     }
+    //     json_start += start_tag.length;
+    //     var json_end = txt.indexOf('</script>', json_start);
+    //     if (json_end === -1) {
+    //         return null;
+    //     }
+    //     var map_data = txt.substring(json_start, json_end);
+    //     map_data = JSON.parse(map_data);
+    //     console.log(map_data);
+    //     if (!('request' in map_data)) {
+    //         return null;
+    //     }
+    //     var name = 'YandexMap';
+    //     var segments = [];
+    //     var error;
+    //     if (map_data.vpage && map_data.vpage.data && map_data.vpage.data.objects && map_data.vpage.data.objects.length) {
+    //         var mapName = ('' + (map_data.vpage.data.name || '')).trim();
+    //         if (mapName.length > 3) {
+    //             name = '';
+    //         } else if (mapName.length) {
+    //             name += ': ';
+    //         }
+    //         name += fileutils.decodeUTF8(mapName);
+    //         map_data.vpage.data.objects.forEach(function(obj){
+    //             if (obj.pts && obj.pts.length) {
+    //                 var segment = [];
+    //                 for (var i=0; i< obj.pts.length; i++) {
+    //                     var pt = obj.pts[i];
+    //                     var lng = parseFloat(pt[0]);
+    //                     var lat = parseFloat(pt[1]);
+    //                     if (isNaN(lat) || isNaN(lng)) {
+    //                         error = 'CORRUPT';
+    //                         break;
+    //                     }
+    //                     segment.push({lat: lat, lng:lng});
+    //                 }
+    //                 if (segment.length) {
+    //                     segments.push(segment);
+    //                 }
+    //             }
+    //         });
+    //     }
+    //     if (map_data.request.args && map_data.request.args.rl) {
+    //         var res = parseYandexRulerString(map_data.request.args.rl);
+    //         error = error || res.error;
+    //         if (res.points && res.points.length) {
+    //             segments.push(res.points);
+    //         }
+    //     }
+    //     return [{name: name, error: error, tracks: segments}];
+    // }
 
     function decodeUrlSafeBase64(s) {
         var decoded;
@@ -450,8 +450,8 @@
             parseGpx,
             parseOziPlt,
             parseKml,
-//            parseYandexRulerUrl,
-            parseYandexMap
+            parseYandexRulerUrl,
+//            parseYandexMap
         ];
         for (var i=0; i<parsers.length; i++) {
             var parsed = parsers[i](data, name);
