2aabee3354d4382b5af62417d234309e4470758a
Sergej Orlov <wladimirych@gmail.com>
2016-10-24 11:32:09 +0300

    changed deploy script for new server

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 make-js-app
 cp src/images/favicon.ico build/production/
-rsync --delete -v -r --owner --chown=www-data:root --perms --chmod=D500,F400 build/production/ root@vultr.wladich.tk:/var/www/nakarte
+rsync --delete -v -r --owner --chown=www-data:root --perms --chmod=D500,F400 build/production/ root@time4vps.wladich.tk:/var/www/nakarte
 
